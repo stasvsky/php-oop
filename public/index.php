44ec197054c71628ebab4cf06f5e04000dfb1158
s.vasilewskiy <s.vasilewskiy@gmail.com>
2024-05-06 23:34:06 +0300

    added mvc

--- public/index.php
+++ public/index.php
@@ -2,12 +2,21 @@
 
 require_once __DIR__. '/../vendor/autoload.php';
 
+session_start();
+
+define('STORAGE_PATH', __DIR__ . '/../storage');
+define('VIEW_PATH', __DIR__ . '/../views');
+
 $router = new App\Router();
 
 $router
-    ->get('/', [App\Classes\Home::class, 'index'])
-    ->get('/invoices', [App\Classes\Invoice::class, 'index'])
-    ->get('/invoices/create', [App\Classes\Invoice::class, 'create'])
-    ->post('/invoices/create', [App\Classes\Invoice::class, 'store']);
+    ->get('/', [App\Controllers\HomeController::class, 'index'])
+    ->post('/upload', [App\Controllers\HomeController::class, 'upload'])
+    ->get('/invoices', [App\Controllers\InvoiceController::class, 'index'])
+    ->get('/invoices/create', [App\Controllers\InvoiceController::class, 'create'])
+    ->post('/invoices/create', [App\Controllers\InvoiceController::class, 'store']);
 
-echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+echo $router->resolve(
+    $_SERVER['REQUEST_URI'],
+    strtolower($_SERVER['REQUEST_METHOD'])
+);
