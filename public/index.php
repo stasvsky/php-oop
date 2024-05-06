800507d224d60b51dd6e1ab9f9dca481b038d88d
s.vasilewskiy <s.vasilewskiy@gmail.com>
2024-05-06 14:35:39 +0300

    implementation of simple routing

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__. '/../vendor/autoload.php';
+
+$router = new App\Router();
+
+$router
+    ->register('/', [App\Classes\Home::class, 'index'])
+    ->register('/invoices', [App\Classes\Invoice::class, 'index'])
+    ->register('/invoices/create', [App\Classes\Invoice::class, 'create']);
+
+echo $router->resolve($_SERVER['REQUEST_URI']);
