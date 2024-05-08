ef41977d866c58be14835e564376a60f310abad0
s.vasilewskiy <s.vasilewskiy@gmail.com>
2024-05-08 10:51:43 +0300

    improved di container

--- app/Container.php
+++ app/Container.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Exceptions\Container\ContainerException;
 use App\Exceptions\Container\NotFoundException;
 use Psr\Container\ContainerInterface;
 
@@ -13,13 +14,14 @@ class Container implements ContainerInterface
 
     public function get(string $id)
     {
-        if (!$this->has($id)) {
-            throw new NotFoundException('Class "' . $id . '" has no binding');
+        if ($this->has($id)) {
+            $entry = $this->entries[$id];
+
+            return $entry($this);
         }
 
-        $entry = $this->entries[$id];
-
-        return $entry($this);
+        // throw new NotFoundException('Class "' . $id . '" has no binding');
+        return $this->resolve($id);
     }
 
     public function has(string $id): bool
@@ -27,8 +29,63 @@ class Container implements ContainerInterface
         return isset($this->entries[$id]);
     }
 
-    public function set(string $id, callable $concrete)
+    public function set(string $id, callable $concrete): void
     {
         $this->entries[$id] = $concrete;
+    }
+
+    public function resolve(string $id)
+    {
+        // 1. Inspect the class that we are trying to get from the container
+        $reflectionClass = new \ReflectionClass($id);
+
+        if (! $reflectionClass->isInstantiable()) {
+            throw new ContainerException('Class "' . $id . '" is not instantiable');
+        }
+
+        // 2. Inspect the constructor of the class
+        $constructor = $reflectionClass->getConstructor();
+
+        if (! $constructor) {
+            return new $id;
+        }
+
+        // 3. Inspect the constructor parameters (dependencies)
+        $parameters = $constructor->getParameters();
+
+        if (! $parameters) {
+            return new $id;
+        }
+
+        // 4. If the constructor parameter is a class then try to resolve that class using the container
+        $dependencies = array_map(
+            function (\ReflectionParameter $param) use ($id) {
+                $name = $param->getName();
+                $type = $param->getType();
+
+                if (! $type) {
+                    throw new ContainerException(
+                        'Failed to resolve class "' . $id . '" because param "' . $name . '" is missing a type hint'
+                    );
+                }
+
+                if ($type instanceof \ReflectionUnionType) {
+                    throw new ContainerException(
+                        'Failed to resolve class "' . $id . '" because of union type for param "' . $name . '"'
+                    );
+                }
+
+                if ($type instanceof \ReflectionNamedType && ! $type->isBuiltin()) {
+                    return $this->get($type->getName());
+                }
+
+                throw new ContainerException(
+                    'Failed to resolve class "' . $id . '" because invalid param "' . $name . '"'
+                );
+            },
+            $parameters
+        );
+
+        return $reflectionClass->newInstanceArgs($dependencies);
     }
 }
--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,31 +4,39 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Models\Invoice;
-use App\Models\SignUp;
-use App\Models\User;
+use App\Container;
+use App\Services\InvoiceService;
 use App\View;
 
 class HomeController
 {
+    public function __construct(private InvoiceService $invoiceService)
+    {
+    }
+
     public function index(): View
     {
-        $email = 'jane22@doe.com';
-        $name = 'Jane22 Doe22';
-        $amount = 1200;
+        // $email = 'jane22@doe.com';
+        // $name = 'Jane22 Doe22';
+        // $amount = 1200;
+        //
+        // $userModel    = new User();
+        // $invoiceModel = new Invoice();
+        // $invoiceId    = (new SignUp($userModel, $invoiceModel))->register(
+        //     [
+        //         'email' => $email,
+        //         'name' => $name,
+        //     ],
+        //     [
+        //         'amount' => $amount
+        //     ]
+        // );
 
-        $userModel    = new User();
-        $invoiceModel = new Invoice();
-        $invoiceId    = (new SignUp($userModel, $invoiceModel))->register(
-            [
-                'email' => $email,
-                'name' => $name,
-            ],
-            [
-                'amount' => $amount
-            ]
-        );
+        // return View::make('index', ['invoice' => $invoiceModel->find($invoiceId)]);
 
-        return View::make('index', ['invoice' => $invoiceModel->find($invoiceId)]);
+
+        $this->invoiceService->process([], 25);
+
+        return View::make('index');
     }
 }
